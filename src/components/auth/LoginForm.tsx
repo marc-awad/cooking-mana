a44dc64170ceb2ee774fb9e603dd9b877ecb325c
LeoChris88 <leochan2005@outlook.fr>
2026-03-05 14:31:54 +0100

    login feature

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Connexion</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">
+        Se connecter
+      </button>
+    </form>
+  );
+}
